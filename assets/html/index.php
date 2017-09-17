c887cd3a394d19e95717f8d8e93bed1473fca2fa
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-09-17 23:37:51 +0000

    Finished _font.scss

--- assets/html/index.php
+++ assets/html/index.php
@@ -1,20 +1,7 @@
 <html>
 <head>
-    <title></title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>My Portfolio</title>
     <link rel="stylesheet" href="css/main.min.css">
-    <style>
-        .container {
-            height: 500px;
-            border: 1px solid #000;
-        }
-        .item {
-            width: 500px;
-            height: 50px;
-            border: 1px solid #000;
-        }
-    </style>
 </head>
 <body>
     <!-- header -->
--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<html><head><title></title><link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link rel="stylesheet" href="css/main.min.css"><style>
-        .container {
-            height: 500px;
-            border: 1px solid #000;
-        }
-        .item {
-            width: 500px;
-            height: 50px;
-            border: 1px solid #000;
-        }
-    </style></head><body><header></header><main></main><footer></footer></body></html>
+<html><head><title>My Portfolio</title><link rel="stylesheet" href="css/main.min.css"></head><body><header></header><main></main><footer></footer></body></html>
